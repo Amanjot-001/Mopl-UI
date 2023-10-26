efc2ab7d791efa1b29f2133c3f2f9d36d6682552
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-27 01:45:43 +0530

    feature added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,21 @@
-export default function Card() {
-    return (
-        <div className="card">
-            <div className="img"></div>
-            <div className="heading"></div>
-        </div>
-    )
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFaceSurprise, faFaceSmileWink, faFaceGrinHearts } from '@fortawesome/free-regular-svg-icons';
+
+const iconMap = {
+  surprise: faFaceSurprise,
+  wink: faFaceSmileWink,
+  heart: faFaceGrinHearts,
+};
+
+export default function Card({ heading, imgSource }) {
+  const icon = iconMap[imgSource.toLowerCase()];
+
+  return (
+    <div className="card">
+      <div className="img">
+        {icon && <FontAwesomeIcon icon={icon} />}
+      </div>
+      <div className="heading">{heading}</div>
+    </div>
+  );
 }
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,26 @@
-export default function Features() {
+import Card from "./Card";
 
+const cardData = [
+    {
+        heading: 'Easy to use',
+        imgSource: 'surprise',
+    },
+    {
+        heading: 'Simple syntax',
+        imgSource: 'wink',
+    },
+    {
+        heading: 'Fast',
+        imgSource: 'heart',
+    },
+]
+
+export default function Features() {
+    return (
+        <div className="features">
+            {cardData.map((card, index) => (
+                <Card key={index} heading={card.heading} imgSource={card.imgSource} />
+            ))}
+        </div>
+    );  
 }
