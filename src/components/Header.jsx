ece580dae65cee8ee1b2abc2c13e5370fe1f034b
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-27 00:12:02 +0530

    header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
+import '../styles/Header.css'
+
 export default function Header() {
     return (
         <div className="header">
             <div className="first">
-                <h1>Code Effecttively</h1>
+                <h1>Code Effectively</h1>
             </div>
             <div className="second">
-                <h3>- with our own programming language.</h3>
+                <h3>
+                    <span className="dash">&#8212;</span>
+                    with our own programming language.
+                </h3>
             </div>
         </div>
     )
