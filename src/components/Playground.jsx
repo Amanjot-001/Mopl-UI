4b8047c9240654a0d4550d8bdd62b2fab852d95b
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-30 14:20:49 +0530

    syntax highlight update

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,37 +1,53 @@
 import '../styles/Playground.css'
-// import Editor from 'react-simple-code-editor'
-import { useState} from 'react';
+import Editor from 'react-simple-code-editor'
+import { useState } from 'react';
 import 'prismjs/themes/prism.css';
-import Prism from 'prismjs';
+import Prism, { highlightAll } from 'prismjs';
 import '../utils/mopl'
 
 export default function Playground() {
     const [code, setCode] = useState('');
-    const [highlightedCode, setHighlightedCode] = useState('');
+    // const [highlightedCode, setHighlightedCode] = useState('');
 
-    const handleCodeChange = (event) => {
-        const newCode = event.target.value;
-        setCode(newCode);
+    // const handleCodeChange = (event) => {
+    //     const newCode = event.target.value;
+    //     setCode(newCode);
 
-        const highlighted = Prism.highlight(newCode, Prism.languages.mopl, 'mopl');
-        setHighlightedCode(highlighted);
+    //     const highlighted = Prism.highlight(newCode, Prism.languages.mopl, 'mopl');
+    //     setHighlightedCode(highlighted);
+    // };
+    const highlightCode = (code) => {
+        return Prism.highlight(code, Prism.languages.mopl, 'mopl');
     };
 
 
     return (
-        <div className="playground">
-            <textarea
-                cols="30"
-                rows="10"
-                value={code}
-                onChange={handleCodeChange}
-                className="language-mopl"
-            ></textarea>
-            <div className="highlighted-code">
-                <pre>
-                    <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
-                </pre>
-            </div>
-        </div>
+        <Editor
+            value={code}
+            onValueChange={code => setCode(code)}
+            highlight={code => highlightCode(code)}
+            padding={'1rem'}
+            style={{
+                fontFamily: 'monospace',
+                fontSize: 14,
+                backgroundColor: '#f5f5f5',
+                border: '1px solid #ccc',
+                borderRadius: 4,
+            }}
+        />
+        // <div className="playground">
+        //     <textarea
+        //         cols="30"
+        //         rows="10"
+        //         value={code}
+        //         onChange={handleCodeChange}
+        //         className="language-mopl"
+        //     ></textarea>
+        //     <div className="highlighted-code">
+        //         <pre>
+        //             <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+        //         </pre>
+        //     </div>
+        // </div>
     )
 }
